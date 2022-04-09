03d3bd2fd5983ac648ff23ea55217a4895e907c3
MohammadRijwan <mohammad.rijwan@gmail.com>
2022-04-09 16:18:09 +0530

    fix(bluetoothVm): removed bluetoothVm

--- lib/flutter_bluetooth.dart
+++ lib/flutter_bluetooth.dart
@@ -6,6 +6,5 @@ export 'package:flutter_bluetooth/src/app_constants.dart';
 export 'package:flutter_bluetooth/src/ble_characteristic.dart';
 export 'package:flutter_bluetooth/src/ble_device.dart';
 export 'package:flutter_bluetooth/src/ble_usecase.dart';
-export 'package:flutter_bluetooth/src/bluetooth_vm.dart';
 export 'package:flutter_bluetooth/src/data_convertor.dart';
 export 'package:flutter_bluetooth/src/flutter_bluetooth_service.dart';
